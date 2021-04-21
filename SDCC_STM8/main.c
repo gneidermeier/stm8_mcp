71431ce24e48e142acce952c39e6fbc04da9ed54
neiderm <neiderm@neiderm>
2021-04-21 15:35:06 -0400

    Updated for stm8s003 support - V2.3.0 / 16-June-2017

--- SDCC_STM8/main.c
+++ SDCC_STM8/main.c
@@ -1,36 +1,51 @@
 #include "stm8s.h"
+#include "stdio.h"
 
-// STM8S105 Discovery on-board LED
-#define LED_GPIO_PORT  (GPIOD)
-#define LED_GPIO_PINS  (GPIO_PIN_0)
+//int putchar(int c);
+//int getchar(void);
 
-void Delay (uint16_t nCount);
-
-int main(void)
+void main(void)
 {
-  uint8_t i_var =  1;
-  uint8_t io = 255;
-  io++;
-  /* Initialize I/Os in Output Mode */
-  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
+
+  char receivedByte;
+    
+  UART1_DeInit();
+
+  /* UART1 needs some settings:
+  115200 as BAUD, 8 bit wordlength, 1 stopbit and no parity.
+  Syncmode clock disabled, and let's enable both RX and TX so printf and getchar works
+  */
+  UART1_Init((uint32_t)115200, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO,
+              UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
+
+  printf("Printf with an STM8S is remarably easy :D\n");
+  printf("Now with the added bonus of GDB debugging :D\n");
+  printf("Enter text now and it will be echoed with a newline added\n");
 
   while (1)
   {
-    /* Toggles LEDs */
-    i_var++;
-    GPIO_WriteReverse(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS);
-    Delay(0xFFFF);
+    receivedByte = getchar();
+    printf("%c\n", receivedByte);
   }
 
 }
 
-void Delay(uint16_t nCount)
+int _putchar(int c) 
 {
-  /* Decrement nCount value */
-  while (nCount != 0)
-  {
-    nCount--;
-  }
+  //Write a character and wait till it's done
+  UART1_SendData8(c);
+  while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET);
+  return 0;
+}
+
+int _getchar(void)
+{
+
+  int c = 0;
+  //Loop until a character can be read
+  while (UART1_GetFlagStatus(UART1_FLAG_RXNE) == RESET);
+    c = UART1_ReceiveData8();
+  return (c);
 }
 
 #ifdef USE_FULL_ASSERT
