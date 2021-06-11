a1fc25e62cbd32873f1f822bb45a0874866021d5
neiderm <neiderm@neiderm>
2021-06-10 21:26:15 -0400

    Eliminate CONTRLLR_TIMER macro

--- src/pwm_stm8s.c
+++ src/pwm_stm8s.c
@@ -91,7 +91,7 @@ void set_dutycycle(uint16_t global_dutycycle)
 /*
  * The S105 dev board unfortunately does not let the TIM2 CH3 pin (unless by alt. fundtion)
  */
-#if (CONTRLLR_TIMER == 2) //  S003 DEV or S105 DISCOVERY
+#if defined ( S105_DISCOVERY ) || defined ( S003_DEV )
 
 /*
  * Setup TIM2 PWM
@@ -103,7 +103,6 @@ void set_dutycycle(uint16_t global_dutycycle)
 #define TIM2_PRESCALER TIM2_PRESCALER_4  //    (1/8Mhz)  * 4 * 250 -> 0.000125 S
 #endif
 
-// this is not wonderful ... could get out of sync with "#if (CONTRLLR_TIMER == 1)" etc. in pwm_stm8s.h
 #define PWM_TIMER_CHAN_A  TIM2_CHANNEL_1
 #define PWM_TIMER_CHAN_B  TIM2_CHANNEL_2
 #define PWM_TIMER_CHAN_C  TIM2_CHANNEL_3
@@ -117,28 +116,20 @@ void PWM_setup(void)
   TIM2_TimeBaseInit(TIM2_PRESCALER, TIM2_PWM_PD);
   /* Channel 1 PWM configuration */
   TIM2_OC1Init(TIM2_OCMODE_PWM2, TIM2_OUTPUTSTATE_ENABLE, 0, TIM2_OCPOLARITY_LOW );
-  TIM2_OC1PreloadConfig(ENABLE);
+//  TIM2_OC1PreloadConfig(ENABLE);
 
   /* Channel 2 PWM configuration */
   TIM2_OC2Init(TIM2_OCMODE_PWM2, TIM2_OUTPUTSTATE_ENABLE, 0, TIM2_OCPOLARITY_LOW );
-  TIM2_OC2PreloadConfig(ENABLE);
+//  TIM2_OC2PreloadConfig(ENABLE);
 
   /* Channel 3 PWM configuration */
   TIM2_OC3Init(TIM2_OCMODE_PWM2, TIM2_OUTPUTSTATE_ENABLE, 0, TIM2_OCPOLARITY_LOW );
-  TIM2_OC3PreloadConfig(ENABLE);
+//  TIM2_OC3PreloadConfig(ENABLE);
 
   /* Enables TIM2 peripheral Preload register on ARR */
-  TIM2_ARRPreloadConfig(ENABLE);
-/** 
- * Enable the ISR - initiates the back-EMF ADC measurement at the starting edge of
- * the PWM pulse. The timer is required to be left free running as it drives the
- * the periodic UI and Controller tasks (unless the timer is explicitly stopped or
- * reset, there is effect from i.e. starting/stopping the PWM output channels as is
- * done for the motor commutation control).
- */
-  TIM2_ITConfig(TIM2_IT_UPDATE, ENABLE); // GN: for eventual A/D triggering
+//  TIM2_ARRPreloadConfig(ENABLE);
 
-  /* Enable TIM2 */
+  TIM2_ITConfig(TIM2_IT_UPDATE, ENABLE);  // for triggering ADC capture
   TIM2_Cmd(ENABLE);
 }
 
@@ -178,29 +169,23 @@ void PWM_PhC_Enable(void)
     TIM2_CCxCmd( PWM_TIMER_CHAN_C, ENABLE );
 }
 
-#else // S003 Dev Board 
+#elif defined ( S105_DEV )
 
-/*
- * Timer 1 Setup
- * from:
- *   http://www.emcu.it/STM8/STM8-Discovery/Tim1eTim4/TIM1eTIM4.html
-*/
 #ifdef CLOCK_16
 #define TIM1_PRESCALER 8  //    (1/16Mhz) * 8 * 250 -> 0.000125 S
 #else
 #define TIM1_PRESCALER 4  //    (1/8Mhz)  * 4 * 250 -> 0.000125 S
-#endif
+#endif // CLOCK_16
 
 #define PWM_MODE  TIM1_OCMODE_PWM2
 
-// this is not wonderful ... could get out of sync with "#if (CONTRLLR_TIMER == 1)" etc. in pwm_stm8s.h
 #define PWM_TIMER_CHAN_A  TIM1_CHANNEL_2
 #define PWM_TIMER_CHAN_B  TIM1_CHANNEL_3
 #define PWM_TIMER_CHAN_C  TIM1_CHANNEL_4
 
 void PWM_setup(void)
 {
-    const uint16_t T1_Period = TIM2_PWM_PD /* TIMx_PWM_PD */ ;  // 16-bit counter
+    const uint16_t T1_Period = TIM2_PWM_PD;  // 16-bit counter
 
 //    CLK_PeripheralClockConfig (CLK_PERIPHERAL_TIMER1, ENABLE);  // with clocks setup
 
@@ -209,8 +194,6 @@ void PWM_setup(void)
  * The counter clock frequency fCK_CNT is equal to fCK_PSC / (PSCR[15:0]+1)  (RM0016)
  */
     TIM1_TimeBaseInit(( TIM1_PRESCALER - 1 ), TIM1_COUNTERMODE_UP, T1_Period, 0); // ISR at and of "idle" time
-
-// BAH fixed names of functions specific to specific channel designation 
 
     /* Channel 2 PWM configuration */
     TIM1_OC2Init( PWM_MODE,
@@ -221,7 +204,6 @@ void PWM_setup(void)
                   TIM1_OCNPOLARITY_LOW,
                   TIM1_OCIDLESTATE_RESET,
                   TIM1_OCNIDLESTATE_RESET);
-    //   TIM2_OC2PreloadConfig(ENABLE); ??
 
     /* Channel 3 PWM configuration */
     TIM1_OC3Init( PWM_MODE,
@@ -232,7 +214,6 @@ void PWM_setup(void)
                   TIM1_OCNPOLARITY_LOW,
                   TIM1_OCIDLESTATE_RESET,
                   TIM1_OCNIDLESTATE_RESET);
-// ?  TIM2_OC3PreloadConfig(ENABLE);
 
     /* Channel 4 PWM configuration */
     TIM1_OC4Init(PWM_MODE,
@@ -243,7 +224,7 @@ void PWM_setup(void)
 
     TIM1_CtrlPWMOutputs(ENABLE);
 
-    TIM1_ITConfig(TIM1_IT_UPDATE, ENABLE);
+    TIM1_ITConfig(TIM1_IT_UPDATE, ENABLE);  // for triggering ADC capture
     TIM1_Cmd(ENABLE);
 }
 /**
@@ -264,9 +245,6 @@ void PWM_PhC_Disable(void)
     TIM1_CCxCmd( PWM_TIMER_CHAN_C, DISABLE );
 }
 
-/*
- * BAH these are specific to the Timer channels used ... grrrrr
- */
 void PWM_PhA_Enable(void)
 {
     TIM1_SetCompare2( global_uDC );
